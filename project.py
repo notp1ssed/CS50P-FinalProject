32218eafd75d89e938651c80a06544a57f075e69
notp1ssed <ruca1995@live.com.pt>
2024-09-05 14:20:29 +0100

    final-version

--- project.py
+++ project.py
@@ -10,23 +10,6 @@ _inf: float = 0.03             # inflation rate
 _r: float = 0.08               # investment rate of return
 
 
-
-
-
-### Guardar valores em dictionary!
-_name: str = ""              # person's name
-_age: int = 0                # current age
-_retirement_age: int = 0     # age at which wishes to retire
-_working_years: int = 0      # no. years that's still going to work 
-_retirement_cf: float = 0      # annual cf adjust today's value for inflation 3% annually
-_retirement_time: float = 0    # total time spent in retirement
-_current_savings: float = 0    # Current savings
-_req_capital: float = 0        # Starting capital needed at the beggining of retirement.
-_req_lumpsum: float = 0        # Required amount to invest today to reach target
-_req_annual_cf: float = 0      # Required annual investment to reach target
-_req_annual_cf_w_savings: float = 0        # Including savings required annual investment to reach target
-
-
 def main():
     print("\n")
     print(figlet.renderText("Welcome!"))
@@ -39,85 +22,112 @@ def main():
 
 
 ############################# Auto report
-def collect_data() -> None:
+def collect_data() -> dict:
     '''
     A function to collect basic user data in order to do calculations later.
+    This will return a dictionary containing all the relevant collected data.
     '''
     print("We need some data before we initialize.".upper())
-    global _name, _age, _retirement_age, _working_years, _retirement_cf, _retirement_time, _current_savings
-    _name = input("\nWhat's your name? ").strip().capitalize()
-    print(f"Hello {_name}. Let's start!")
-    _age = int(input("What's your age? ").strip())
-    _retirement_age = int(input("What's your desired retirement age? ").strip())     # age at which wishes to retire
-    _retirement_time = (float(input("What's your life expectancy? ").strip())) - _retirement_age
-    _working_years = (_retirement_age - _age)   # no. years that's still going to work 
-    _retirement_cf = float(input("When retired, what's your desired monthly cashflow? ").strip())
-    _current_savings = float(input("What's your current savings? ").strip())
-    print("\n\n--------------------collected info--------------------".upper())
-    print("Age:", _age)
-    print("Retirement age:", _retirement_age)
-    print(f"Life expectancy: {_retirement_time + _retirement_age}")
-    print(f"Monthly retirement withdrawals: {_retirement_cf}")
-    print("Current Savings:", _current_savings)
-    ask: str = input("\nIs all this info correct? (y/n) ").strip()
-    if ask == "y":
-        pass
-    elif ask == "n":
-        collect_data() # redo collection of data
-    else:
-        print("Invalid option. Please try again.")
+    
+    while True:
+        _name = input("\nWhat's your name? ").strip().capitalize()
+        print(f"Hello {_name}. Let's start!")
+        _age = int(input("What's your age? ").strip())
+        _retirement_age = int(input("What's your desired retirement age? ").strip())     # age at which wishes to retire
+        _life_expectancy = float(input("What's your life expectancy? ").strip())
+        _retirement_time = (_life_expectancy - _retirement_age)
+        _working_years = (_retirement_age - _age)   # no. years that's still going to work 
+        _retirement_cf = float(input("When retired, what's your desired monthly cashflow? ").strip())
+        _current_savings = float(input("What's your current savings? ").strip())
+        
+        data_dictionary = {"name": _name,                       # person's name
+                            "age": _age,                        # current age
+                            "retirement_age": _retirement_age,  # age at which wishes to retire
+                            "retirement_time": _retirement_time,# total time spent in retirement
+                            "working_years": _working_years,    # no. years that's still going to work
+                            "retirement_cf": _retirement_cf,    # annual cf adjust today's value for inflation 3% annually
+                            "current_savings": _current_savings # Current savings
+                            }
+        
+        
+        print("\n\n--------------------collected info--------------------".upper())
+        print("Name:", _name)
+        print("Age:", _age)
+        print("Retirement age:", _retirement_age)
+        print("Life expectancy:", _life_expectancy)
+        print("Monthly retirement withdrawals:", _retirement_cf)
+        print("Current Savings:", _current_savings)
+        
+        ask: str = input("\nIs all this info correct? (y/n) ").strip().lower()
+        if ask == "y":
+            return data_dictionary      # return breaks the loop
+        elif ask == "n":
+            print("\n\n-----------let's collect data again-----------".upper()) # loop again until break
+        else:
+            print("\n\n-----------Invalid option. Please try again-----------".upper()) # loop again until break
 
 
-def auto_report() -> None:
+def auto_report(dictionary) -> dict:
     '''
+    Takes a dictionary with info as input.
     Function that will do all calculation for you.
+    Returns a dictionary with calculations figures.
     '''
-    global _req_capital, _req_lumpsum, _req_annual_cf, _req_annual_cf_w_savings
-    cf = (_retirement_cf * 12)
+    cf = (dictionary['retirement_cf'] * 12)
     r = (1+_r)/(1+_inf)-1
-    n = _retirement_time
+    n = dictionary['retirement_time']
     m = 1
     # Required capital at the start of retirement
     _req_capital = cf*(1-1/(1+r)**n)/r
     
     # Required amount to invest today to reach target
     fv: float = _req_capital
-    n: int = _working_years
+    n: int = dictionary['working_years']
     _req_lumpsum = fv*(1+r/m)**(-n*m)
 
     # Required annual investment to reach target
     _req_annual_cf = fv/(((1+r)**n-1)/r)
 
     # Including savings required annual investment to reach target
-    svgs = _current_savings
+    svgs = dictionary['current_savings']
     svgs_fv = svgs*(1+r)**n     # Calculate Future value of current savings
     rmng_fv = (fv - svgs_fv)    # subtract future value of savings from required capital at retirement beggining
     _req_annual_cf_w_savings = rmng_fv/(((1+r)**n-1)/r)
 
+    data_dictionary = {"req_capital": _req_capital,                 # Starting capital needed at the beggining of retirement.    
+                        "req_lumpsum": _req_lumpsum,                # Required amount to invest today to reach target
+                        "req_annual_cf": _req_annual_cf,                      # Required annual investment to reach target
+                        "req_annual_cf_w_savings": _req_annual_cf_w_savings   # Including savings required annual investment to reach target
+                        }
 
-def generate_report() -> str:
+    return data_dictionary
+
+
+def generate_report(info_dictionary, calculations_dictionary) -> str:
     '''
+    Takes an info dictionary containing basic user data. Takes a second dictionary containing calculations data.
     A function that diggests all the calculations into a simple way of understanding.
+    Returns a formatted string displaying the output in a human readable format.
     '''
     report = ""
     report += "\n\n--------------------auto report--------------------\n".upper()
-    report += f"Hello again {_name} this is your retirement planning report.\n"
+    report += f"Hello again {info_dictionary['name']} this is your retirement planning report.\n"
     report += f"\nassumptions\n".upper()
     report += f"Analysed inflation rate: {_inf}\n"
     report += f"Real rate of investment return {(1+_r)/(1+_inf)-1:.3f}"
     report += "\n\n"
-    report += f"At {_age} you have {_current_savings} in savings.\n"
-    report += f"You expect to retire at age {_retirement_age}, {_working_years} years from now.\n"
-    report += f"In order for you to withdraw monthly {_retirement_cf} in your retirement, you need {_req_capital:.2f} at the beginning of your retirement.\n"
+    report += f"At {info_dictionary['age']} you have {info_dictionary['current_savings']} in savings.\n"
+    report += f"You expect to retire at age {info_dictionary['retirement_age']}, {info_dictionary['working_years']} years from now.\n"
+    report += f"In order for you to withdraw monthly {info_dictionary['retirement_cf']} in your retirement, you need {calculations_dictionary['req_capital']:.2f} at the beginning of your retirement.\n"
     report += "To reach that target you have 3 options:\n\n"
-    report += f"1-) Invest today {_req_lumpsum:.2f} at {_r} nominal rate of return, and let compound interest do the heavy lifting.\n"
-    report += f"2-) Invest annually {_req_annual_cf:.2f} at {_r} nominal rate of return.\n"
-    report += f"3-) Or invest today your current savings ({_current_savings}) and Invest annually ({_req_annual_cf_w_savings}) at {_r} nominal rate of return.\n"
+    report += f"1-) Invest today {calculations_dictionary['req_lumpsum']:.2f} at {_r} nominal rate of return, and let compound interest do the heavy lifting.\n"
+    report += f"2-) Invest annually {calculations_dictionary['req_annual_cf']:.2f} (monthly {(calculations_dictionary['req_annual_cf']/12):.2f}) at {_r} nominal rate of return.\n"
+    report += f"3-) Or invest today your current savings ({info_dictionary['current_savings']}) and Invest annually {calculations_dictionary['req_annual_cf_w_savings']:.2f} (monthly {(calculations_dictionary['req_annual_cf_w_savings']/12):.2f}) at {_r} nominal rate of return.\n"
     report += "\nComparison of the 3".upper()
     report += "\n"
-    report += f"Option 1-) total investment is {_req_lumpsum:.2f}, you only invest one time, today.\n"
-    report += f"Option 2-) total investment is {(_req_annual_cf*_working_years):.2f}, you invest annually {_working_years} times.\n"
-    report += f"Option 3-) total investment is {((_req_annual_cf_w_savings*_working_years)+_current_savings):.2f}, you invest your savings today and invest annually {_working_years} times.\n"
+    report += f"Option 1-) total investment is {calculations_dictionary['req_lumpsum']:.2f}, you only invest one time, today.\n"
+    report += f"Option 2-) total investment is {(calculations_dictionary['req_annual_cf'] * info_dictionary['working_years']):.2f}, you invest annually {info_dictionary['working_years']} times.\n"
+    report += f"Option 3-) total investment is {((calculations_dictionary['req_annual_cf_w_savings'] * info_dictionary['working_years']) + info_dictionary['current_savings']):.2f}, you invest your savings today and invest annually {info_dictionary['working_years']} times.\n"
     
     return report
 
@@ -219,15 +229,15 @@ def change_defaults() -> None:
         print("Invalid option. Please try again.")
 
 
-def save_report(string) -> None:
-    with open(f"{_name}_retirement_planning.txt", "w") as txt_file:
+def save_report(string, info_dictionary) -> None:
+    with open(f"{info_dictionary['name']}_retirement_planning.txt", "w") as txt_file:
         txt_file.write(string)
 
 
 def menu() -> None:
     # Infinit cycle
     while True: 
-        print("\nWelcome!")
+        print("Menu options!")
         print("'1.' Full Report.")
         print("'2.' Calculate required return from x to y in z years.")
         print("'3.' Calculate required time z for x compounds until y.")
@@ -240,12 +250,14 @@ def menu() -> None:
 
         option = input("\nIntroduce the option to execute: ")
         if option == "1":
-            collect_data()
-            auto_report()
-            print(generate_report())
+            collected_data = collect_data()
+            calculations = auto_report(collected_data)
+            generated_report = generate_report(info_dictionary=collected_data, calculations_dictionary=calculations)
+            print(generated_report)
+
             save = input("Do you want to save report as txt file? (y/n) ")
             if save == 'y':
-                save_report(generate_report())
+                save_report(string=generated_report, info_dictionary=collected_data)
             else:
                 pass
         elif option == "2":
